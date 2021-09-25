05d8f93442d6640d1c31fbd78fc63b57cc1a044b
Erich Loftis <erichlof@swbell.net>
2021-09-24 23:07:04 -0500

    Update Test.js

--- js/Test.js
+++ js/Test.js
@@ -1,3 +1,3 @@
 let infoElement = document.getElementById("info");
-infoElement.style.color = "rgb(0,0,255)";
+infoElement.style.color = "rgb(255,0,255)";
 infoElement.innerHTML = "Test.js file says: \"Hello World\"";
