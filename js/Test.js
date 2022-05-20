e5177655bf1e4cb4a4839844965a568b8f28ceff
Erich Loftis <erichlof@swbell.net>
2022-05-20 05:41:49 +0000

    testing change commit

--- js/Test.js
+++ js/Test.js
@@ -1,3 +1,3 @@
 let infoElement = document.getElementById("info");
 infoElement.style.color = "rgb(255,0,255)";
-infoElement.innerHTML = "Test.js file says: \"Hello World\"";
+infoElement.innerHTML = "Test.js file says: \"Hello World!\"";
